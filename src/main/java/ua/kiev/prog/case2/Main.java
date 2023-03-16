268bc963802db35ba3daa236571bb702dfe2cf50
Vladimir Proshin <VlaShin1986@gmail.com>
2023-03-16 22:23:36 +0200

    Initial commit

---
+++ src/main/java/ua/kiev/prog/case2/Main.java
@@ -0,0 +1,58 @@
+package ua.kiev.prog.case2;
+
+import ua.kiev.prog.shared.Client;
+import ua.kiev.prog.shared.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try (Connection conn = ConnectionFactory.getConnection())
+        {
+            // remove this
+            try {
+                try (Statement st = conn.createStatement()) {
+                    st.execute("DROP TABLE IF EXISTS Clients");
+//                    st.execute("CREATE TABLE Clients (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, age INT)");
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            ClientDAOImpl2 dao = new ClientDAOImpl2(conn, "Clients");
+            dao.createTable(Client.class);
+
+            Client c = new Client("test", 1);
+            Client c1 = new Client("Kate", 18);
+            Client c2= new Client("John", 56);
+            Client c3 = new Client("Megan", 35);
+            dao.add(c);
+            dao.add(c1);
+            dao.add(c2);
+            dao.add(c3);
+            // int id = c.getId(); // DZ1
+
+            List<Client> list = dao.getAll(Client.class);
+            for (Client cli : list)
+                System.out.println(cli);
+
+            list.get(0).setAge(55);
+            dao.update(list.get(0));
+            dao.getListByParameters(Client.class, "id", "name");
+
+            /* Dz2
+
+            List<Client> list = dao.getAll(Client.class, "name", "age");
+            List<Client> list = dao.getAll(Client.class, "age");
+            for (Client cli : list)
+                System.out.println(cli);
+
+             */
+
+            dao.delete(list.get(0));
+        }
+    }
+}
